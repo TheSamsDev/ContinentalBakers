d349d6f43daa49749152c96e5612dfd0e2be9d73
thesamsdev <ra6767167@gmail.com>
2025-01-11 00:11:50 +0500

    implemt leaf let api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            RolesAndPermissionsSeeder::class,
-            ProductsSeeder::class,
-            // StoresSeeder::class,
+            // RolesAndPermissionsSeeder::class,
+            // ProductsSeeder::class,
+            StoresSeeder::class,
             
         ]);
     }
